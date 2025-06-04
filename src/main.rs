7b546c63c13fb64bef41ac0a60022f65fad733ec
Don Garrett <dgarrett@acm.org>
2025-06-04 13:10:16 -0500

    Use mdns-sd to discover wled devices.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use mdns_sd::{ServiceDaemon, ServiceEvent};
+
 fn main() {
-    println!("Hello, world!");
+    // Create a daemon
+    let mdns = ServiceDaemon::new().expect("Failed to create daemon");
+
+    // Browse for a service type.
+    let service_type = "_wled._tcp.local.";
+    let receiver = mdns.browse(service_type).expect("Failed to browse");
+
+    // Receive the browse events in sync or async. Here is
+    // an example of using a thread. Users can call `receiver.recv_async().await`
+    // if running in async environment.
+    std::thread::spawn(move || {
+        while let Ok(event) = receiver.recv() {
+            match event {
+                ServiceEvent::ServiceResolved(info) => {
+                    println!("Resolved a new service: {}", info.get_fullname());
+                }
+                other_event => {
+                    println!("Received other event: {:?}", &other_event);
+                }
+            }
+        }
+    });
+
+    // Gracefully shutdown the daemon.
+    std::thread::sleep(std::time::Duration::from_secs(4));
+    mdns.shutdown().unwrap();
 }
