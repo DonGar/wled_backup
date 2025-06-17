c8b2e2680b274664d3060797a664c1cac5930f75
Don Garrett <dgarrett@acm.org>
2025-06-17 11:12:30 -0500

    This is a minimal proof of concept (untested).
    It changes discover to be sync, and adds a backup
    implmentation for discovered devices.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
-use mdns_sd::{ServiceDaemon, ServiceEvent};
+use std::fs::File;
+use std::io::copy;
 
-fn main() {
+use mdns_sd::{ServiceDaemon, ServiceEvent, ServiceInfo};
+
+fn discover_wleds() -> Vec<ServiceInfo> {
+    let mut wleds: Vec<ServiceInfo> = Vec::new();
+
     // Create a daemon
     let mdns = ServiceDaemon::new().expect("Failed to create daemon");
 
@@ -8,23 +13,45 @@ fn main() {
     let service_type = "_wled._tcp.local.";
     let receiver = mdns.browse(service_type).expect("Failed to browse");
 
-    // Receive the browse events in sync or async. Here is
-    // an example of using a thread. Users can call `receiver.recv_async().await`
-    // if running in async environment.
-    std::thread::spawn(move || {
-        while let Ok(event) = receiver.recv() {
-            match event {
-                ServiceEvent::ServiceResolved(info) => {
-                    println!("Resolved a new service: {}", info.get_fullname());
-                }
-                other_event => {
-                    println!("Received other event: {:?}", &other_event);
-                }
+    let search_duration = std::time::Duration::from_secs(4);
+
+    while let Ok(event) = receiver.recv_timeout(search_duration) {
+        match event {
+            ServiceEvent::ServiceResolved(info) => {
+                wleds.push(info.clone());
+                println!("Discovered: {}", info.get_fullname());
+            }
+            _other_event => {}
+        }
+    }
+
+    return wleds;
+}
+
+fn download_file(url: &str, path: &str) -> Result<(), Box<dyn std::error::Error>> {
+    let response = reqwest::blocking::get(url)?;
+    let mut dest = File::create(path)?;
+    let mut content = response;
+    copy(&mut content, &mut dest)?;
+    Ok(())
+}
+
+fn backup_wleds(wleds: Vec<ServiceInfo>) {
+    for wled in wleds.iter() {
+        if let Some(ip) = wled.get_addresses().iter().next() {
+            let url = format!("http://{ip}/presets.json");
+            let file = format!("{}.json", wled.get_hostname());
+            if let Err(result) = download_file(&url, &file) {
+                eprintln!("Failed to backup {}: {result}", wled.get_hostname());
+            } else {
+                println!("Backed up {}: {url} -> {file}", wled.get_hostname());
             }
         }
-    });
+    }
+}
 
-    // Gracefully shutdown the daemon.
-    std::thread::sleep(std::time::Duration::from_secs(4));
-    mdns.shutdown().unwrap();
+fn main() {
+    let wleds = discover_wleds();
+    backup_wleds(wleds);
+    println!("Finished");
 }
